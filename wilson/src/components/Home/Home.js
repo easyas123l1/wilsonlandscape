a4aeebe6a8acbf3a888adc5e555600a3b910452c
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-29 17:39:20 -0500

    a few images client wants to use

--- wilson/src/components/Home/Home.js
+++ wilson/src/components/Home/Home.js
@@ -4,7 +4,9 @@ import landscape from "../../styles/landscape.module.scss";
 export default function Home() {
   return (
     <div className={landscape.background}>
-      <p>Wilson Landscape Home</p>
+      <p>LOL</p>
+      <img src="https://andersonlandscapemain.com/wp-content/uploads/2018/03/L1.png" />
+      <img src="https://andersonlandscapemain.com/wp-content/uploads/2012/03/thumbtack-1.png" />
     </div>
   );
 }
