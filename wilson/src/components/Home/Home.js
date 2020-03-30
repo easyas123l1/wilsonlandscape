1f7616257ffe86e1dc4a33c26434d3672efbb27e
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-30 00:09:24 -0500

    add alt to images

--- wilson/src/components/Home/Home.js
+++ wilson/src/components/Home/Home.js
@@ -5,8 +5,14 @@ export default function Home() {
   return (
     <div className={landscape.background}>
       <p>LOL</p>
-      <img src="https://andersonlandscapemain.com/wp-content/uploads/2018/03/L1.png" />
-      <img src="https://andersonlandscapemain.com/wp-content/uploads/2012/03/thumbtack-1.png" />
+      <img
+        src="https://andersonlandscapemain.com/wp-content/uploads/2018/03/L1.png"
+        alt="Wilson Landscape Thumbtack Top Pro 2017"
+      />
+      <img
+        src="https://andersonlandscapemain.com/wp-content/uploads/2012/03/thumbtack-1.png"
+        alt="Wilson Landscape Thumbtack Best Of 2016"
+      />
     </div>
   );
 }
