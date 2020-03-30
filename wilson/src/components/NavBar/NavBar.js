b6da982ed059d372e81b4cfd750ac4b50a61ca86
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-29 22:34:41 -0500

    navbar styling is done

--- wilson/src/components/NavBar/NavBar.js
+++ wilson/src/components/NavBar/NavBar.js
@@ -4,23 +4,23 @@ import landscape from "../../styles/landscape.module.scss";
 
 export default function NavBar() {
   return (
-    // <div className={landscape.navBar}>
-    //   <img
-    //     className={landscape.headImage}
-    //     src="header.jpg"
-    //     alt="Wilson Landscape 314-853-9585 or 636-332-4343"
-    //   />
-    <div className={landscape.navBarLinks}>
-      <div className={landscape.navDiv}>
-        <Link to="/">About</Link>
-      </div>
-      <div className={landscape.navDiv}>
-        <Link to="/portfolio">Portfolio</Link>
-      </div>
-      <div className={landscape.navDiv}>
-        <Link to="/contact">Contact</Link>
+    <div className={landscape.navBar}>
+      <img
+        className={landscape.headImage}
+        src="newheader.jpg"
+        alt="Wilson Landscape 314-853-9585 or 636-332-4343"
+      />
+      <div className={landscape.navBarLinks}>
+        <div className={landscape.navDiv}>
+          <Link to="/">About</Link>
+        </div>
+        <div className={landscape.navDiv}>
+          <Link to="/portfolio">Portfolio</Link>
+        </div>
+        <div className={landscape.navDiv}>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
     </div>
-    // </div>
   );
 }
