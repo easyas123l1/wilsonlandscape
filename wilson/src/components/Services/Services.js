2870f5394077301f9fd9ef1413bab1c8106f2aeb
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-30 13:41:08 -0500

    add services page

---
+++ wilson/src/components/Services/Services.js
@@ -0,0 +1,10 @@
+import React from "react";
+import landscape from "../../styles/landscape.module.scss";
+
+export default function Services() {
+  return (
+    <div className={landscape.background}>
+      <p>Wilson Landscape Services</p>
+    </div>
+  );
+}
