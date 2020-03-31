a8bef713fbd9202bb9f77a2e84d527beb733742d
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-31 13:47:00 -0500

    compare changes

--- wilson/src/components/Portfolio/ImageCard.js
+++ wilson/src/components/Portfolio/ImageCard.js
@@ -3,9 +3,5 @@ import landscape from "../../styles/landscape.module.scss";
 
 export default function ImageCard({ image }) {
   let source = `./images/image (${image}).jpg`;
-  return (
-    <div>
-      <img src={source} alt="wilson landscape work" />
-    </div>
-  );
+  return <img src={source} alt="wilson landscape work" />;
 }
