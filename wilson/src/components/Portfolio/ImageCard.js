186389d2d2cc0c60bacd605b5fcbfa6ee97c29a2
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-31 17:06:35 -0500

    add a tag that opens new tab for images

--- wilson/src/components/Portfolio/ImageCard.js
+++ wilson/src/components/Portfolio/ImageCard.js
@@ -2,5 +2,9 @@ import React from "react";
 
 export default function ImageCard({ image }) {
   let source = `./images/image (${image}).jpg`;
-  return <img src={source} alt="wilson landscape work" />;
+  return (
+    <a href={source} target="_blank" rel="noopener noreferrer">
+      <img src={source} alt="wilson landscape work" />
+    </a>
+  );
 }
