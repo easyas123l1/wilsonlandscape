fe5a305d35d2e57b58b3a082c737c27507f4efa1
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-03-31 13:49:36 -0500

    remove unneeded import

--- wilson/src/components/Portfolio/ImageCard.js
+++ wilson/src/components/Portfolio/ImageCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import landscape from "../../styles/landscape.module.scss";
 
 export default function ImageCard({ image }) {
   let source = `./images/image (${image}).jpg`;
