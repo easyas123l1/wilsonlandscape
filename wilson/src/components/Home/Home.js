d7e32b376cc863a1c4bb4ba1e8f76adc42285c34
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-04-06 06:09:21 -0500

    fix html

--- wilson/src/components/Home/Home.js
+++ wilson/src/components/Home/Home.js
@@ -8,6 +8,7 @@ export default function Home() {
         <div className={landscape.homeDivide}>
           <div className={landscape.sideOne}>
             <img
+              className={landscape.image}
               src="https://andersonlandscapemain.com/wp-content/uploads/2018/03/L1.png"
               alt="Wilson Landscape Thumbtack Top Pro 2017"
             />
@@ -31,34 +32,11 @@ export default function Home() {
               aliqua eu. 12, 140, 96
             </p>
             <img
+              className={landscape.image}
               src="https://andersonlandscapemain.com/wp-content/uploads/2012/03/thumbtack-1.png"
               alt="Wilson Landscape Thumbtack Best Of 2016"
             />
-            <p>
-              Ad sunt ipsum minim est laborum ipsum minim consectetur elit.
-              Voluptate tempor nulla nisi est velit labore est culpa laboris. Eu
-              anim enim ullamco amet culpa deserunt nostrud consectetur labore
-              officia tempor commodo commodo. Eiusmod est irure aliqua ex quis
-              labore reprehenderit et ad duis sunt pariatur. Ut adipisicing
-              consectetur minim nostrud. Ipsum id aliquip mollit adipisicing.
-              Dolore occaecat reprehenderit velit nostrud commodo mollit aliqua
-              dolor cupidatat. Commodo adipisicing labore culpa non ad officia.
-              Tempor aliquip eiusmod elit minim anim sit esse do culpa velit.
-              Dolore fugiat id est elit exercitation duis duis consequat
-              reprehenderit. Esse duis eiusmod id esse anim velit ea commodo
-              proident pariatur nostrud sint. Tempor quis sit deserunt
-              adipisicing anim dolore. Do aute tempor officia velit. Sit
-              pariatur enim irure ea id mollit velit nulla. Id sint tempor sint
-              est in pariatur. Dolore irure sint incididunt magna sunt veniam
-              veniam eu. Labore nulla reprehenderit amet irure qui elit tempor
-              tempor laboris veniam adipisicing. Culpa id minim incididunt
-              labore consequat culpa amet fugiat aute anim voluptate eiusmod.
-              Duis exercitation ut aute proident aute do amet fugiat laboris.
-              Occaecat mollit proident aliqua ex duis. In mollit exercitation
-              officia laborum consequat eiusmod dolore esse qui veniam aute
-              pariatur minim laboris. Consectetur tempor proident laborum ut
-              proident id fugiat.
-            </p>
+            <p>Ad sunt ipsum minim est laborum ipsum minim consectetur elit.</p>
           </div>
         </div>
       </div>
