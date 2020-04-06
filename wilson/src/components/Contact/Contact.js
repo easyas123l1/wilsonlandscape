49081358d23ec7ba37d3f15c2fc31b6b1f716ac9
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-04-06 03:47:27 -0500

    html and styling for contact page

--- wilson/src/components/Contact/Contact.js
+++ wilson/src/components/Contact/Contact.js
@@ -5,7 +5,42 @@ export default function Contact() {
   return (
     <div className={landscape.background}>
       <div className={landscape.contextDiv}>
-        <p>Wilson Landscape Contact</p>
+        <div className={landscape.contact}>
+          <p>Wilson Landscape Contact</p>
+          <p>
+            Aliqua tempor occaecat voluptate consequat enim ut fugiat qui
+            occaecat. Consequat nisi cupidatat esse et minim elit enim deserunt
+            dolor cupidatat amet proident. Aliqua nostrud commodo ipsum laboris
+            eu minim exercitation do veniam adipisicing eiusmod. Est proident
+            dolor nisi elit consectetur id excepteur laboris et eiusmod. Ex
+            eiusmod ipsum excepteur consequat exercitation ut laboris mollit
+            non. Velit eiusmod veniam do labore. Deserunt incididunt eu esse id
+            excepteur Lorem duis cupidatat anim amet ea aliquip. Qui id aliquip
+            est duis. Esse sint aliquip Lorem cupidatat ullamco laborum deserunt
+            culpa aliquip tempor. Proident consectetur adipisicing et enim et
+            eiusmod do voluptate do ipsum. Pariatur aute nostrud consequat
+            nostrud id id aliqua pariatur elit excepteur dolore. Consequat quis
+            duis dolore id commodo ullamco ut aute commodo voluptate elit
+            reprehenderit ad cillum. Tempor excepteur consequat non consectetur
+            magna cupidatat duis sit. Incididunt non commodo consequat minim
+            consectetur consequat eiusmod esse sint id occaecat quis voluptate
+            culpa. Do proident id quis irure qui proident mollit non voluptate
+            reprehenderit duis pariatur pariatur. Cupidatat reprehenderit
+            incididunt minim mollit consectetur aliqua sunt ex sunt.
+            Reprehenderit sunt duis veniam nulla mollit enim ut occaecat do
+            anim. Esse nulla exercitation consequat sint. Ea nostrud do deserunt
+            magna laboris cupidatat laborum commodo eu aliquip mollit laboris
+            ea. Duis quis aliquip cillum aute ad magna excepteur ea. Non
+            proident nulla dolor incididunt occaecat. Occaecat dolor occaecat
+            excepteur laborum proident qui ea dolor dolor id duis laboris dolore
+            qui.
+          </p>
+          <img
+            className={landscape.image2}
+            src="./images/image (30).jpg"
+            alt="Wilson Landscape Vehicles on jobsite"
+          />
+        </div>
       </div>
     </div>
   );
